48a6faa5f9ffcc9969cd368ba3f635038156956d
chris Valle(Linux) <christophervalle3338@gmail.com>
2025-10-05 22:10:12 -0700

    Created product model with all attributes && added JPA dependency

---
+++ src/main/java/com/chrisV/InventorySystem/model/Product.java
@@ -0,0 +1,25 @@
+package com.chrisV.InventorySystem.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer stock;
+    private Double Price;
+    private Double Total;
+    private String code;
+}
